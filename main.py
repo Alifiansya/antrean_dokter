91d612c01f337c928813f527a72a0220b9181b1b
Alifiansya <alifiansyahfirdaus@gmail.com>
2022-10-28 21:15:19 +0700

    data antrean masuk

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
+import os
 from art import *
 from tabulate import tabulate
 from time import sleep
 import pandas as pd
-import pasien as ps
+from pasien import Pasien
 
 if __name__ == "__main__":
+    pasien = Pasien()
     while True:
-        print("Data antrian: ")
+        print("Data antrian dr.Samsudin: ")
         # Print table dari csv
-        df = pd.read_csv("data_antrean.csv")
-        print(tabulate(df, headers = 'keys', tablefmt = 'fancy_grid')) 
+        df1 = pd.read_csv("data_antrean/samsudin.csv")
+        print(tabulate(df1, headers = 'keys', tablefmt = 'fancy_grid')) 
+
+        print("Data antrian dr.Budi: ")
+        # Print table dari csv
+        df2 = pd.read_csv("data_antrean/budi.csv")
+        print(tabulate(df2, headers = 'keys', tablefmt = 'fancy_grid')) 
+
+        print("Data antrian dr.Supratman: ")
+        # Print table dari csv
+        df3 = pd.read_csv("data_antrean/supratman.csv")
+        print(tabulate(df3, headers = 'keys', tablefmt = 'fancy_grid')) 
+
         match input("Apa yang ingin anda lakukan?: "):
             # Mengambil data dari console
             case "tambah data":
-                data = ['nama', 'Nomor Handphone']
-                for i in range(len(data)):
-                    data[i] = input(f"Masukan {data[i]}")
-                print("Nama Dokter yang tersedia: ")
-                dr = ["dr.Supratman", "dr.Budi", "dr.Samsudin"]
-                for i in range(len(dr)):
-                    print(f"{i+1}. {dr[i]}")
-                data.append(dr[int(input("Pilih dokter yang akan ditemui: ")) - 1])
-                if df.empty:
-                    data.insert(0, 1)
-                else: 
-                    # Mengambil id data terakhir di csv
-                    data.insert(0, int(df.iloc[-1][0])+1)
-                pasien = ps.Pasien(data)
+                pasien.ambil_data()
                 pasien.masuk_csv()
             case "next":
-                data_front = list(df.iloc[0])
-                print("\n")
-                print(text2art(f"{data_front[0]}", font="block", chr_ignore=False))
-                sleep(10)
-                print("\n")
-                df = df[df.index != 0]
-                df.to_csv("data_antrean.csv", index=False)
+                pass
             case default:
                 print("coba lagi!")
--- pasien.py
+++ pasien.py
@@ -1,9 +1,35 @@
+import json
+
+with open("dokter.json") as f:
+    dokter = json.load(f)
+nama_dokter = [x['nama'] for x in dokter]
 
 class Pasien:
-    def __init__(self, data: list):
-        self.data = data
+    def __init__(self) -> None:
+        pass
+    def ambil_data(self):
+        self.nama = input("Masukan nama: ")
+        self.hp = input("Masukan Nomor HP: ")
+        print(
+            "Dokter yang ada:\n",
+            "1. Dr.Samsudin\n",
+            "2. Dr.Supratman\n",
+            "3. Dr.Budi"
+        )
+        dokter_pilihan = input("Pilih nomor dokter yang ada: ")
+        self.drID = int(dokter_pilihan) - 1
+        
     def masuk_csv(self):
-        data_csv = ','.join(str(x) for x in self.data)
-        with open("data_antrean.csv", 'a') as f:
-            f.write(f"\n{data_csv}")
+        with open("nomor_antre.txt", 'r+') as f:
+            data = f.read().split(',')
+            print(data)
+            data[self.drID] = int(data[self.drID]) + 1
+            self.no_antre = data[self.drID]
+            data[self.drID] = str(data[self.drID])
+            f.write(','.join(data))
 
+        with open(f"data_antrean/{nama_dokter[self.drID][3:].lower()}.csv", 'a') as f:
+            data_csv = ','.join([str(self.no_antre), self.nama, self.hp, nama_dokter[self.drID]])
+            f.write(f"\n{data_csv}")
+        
+    
