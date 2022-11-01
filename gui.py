1fada7a1667ca15764e206dfd1936e56925f9a56
Alifiansya <alifiansyahfirdaus@gmail.com>
2022-11-02 05:24:13 +0700

    added submit_data, metod yang masukin data ke csv

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
-from time import sleep
 from tkinter import *
 from tkinter import ttk
-
+from time import sleep
+import csv
 
 class InputDataWin:
     def __init__(self):
@@ -23,7 +23,7 @@ class InputDataWin:
         self.nama = StringVar()
         self.alamat = StringVar()
         self.noTelp = StringVar()
-        self.dokter = StringVar()
+        self.dokter = StringVar(None, "tulus")
         
         ttk.Entry(mainframe, width=28, textvariable=self.nama).grid(row=1, column=3,columnspan=3, sticky=W)
         ttk.Entry(mainframe, width=28, textvariable=self.alamat).grid(row=2, column=3,columnspan=3, sticky=W)
@@ -31,8 +31,25 @@ class InputDataWin:
         ttk.Radiobutton(mainframe, text="dr. Tulus", value="tulus", variable=self.dokter).grid(row=4, column=3, sticky=W)
         ttk.Radiobutton(mainframe, text="dr. Gisel", value="gisel", variable=self.dokter).grid(row=5, column=3,sticky=W)
 
-        ttk.Button(mainframe, text="Submit", command=self.root.destroy).grid(row=4, column=5, rowspan=2)
+        ttk.Button(mainframe, text="Submit", command=self.submit_data).grid(row=4, column=5, rowspan=2)
     
+    def submit_data(self, *args):
+        drId = 0 if self.dokter.get() == "tulus" else 1
+        with open("no_antre.txt", "r") as f:
+            no_antre = list(map(int, f.read().split(',')))
+            no_antre[drId] += 1
+            with open("no_antre.txt", "w") as fwrite:
+                fwrite.write(','.join(list(map(str, no_antre))))
+
+        isidata = [no_antre[drId], self.nama.get(), self.alamat.get(), self.noTelp.get()]
+        with open(f"{self.dokter.get()}.csv", 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(isidata)
+
+
+
+
+
 if __name__ == "__main__":
     inputWin = InputDataWin()
     inputWin.root.mainloop()
