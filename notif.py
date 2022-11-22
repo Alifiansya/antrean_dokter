fbe3288623eda5fd74a309a9ed9ff0a2a19eae4f
Alifiansya <alifiansyahfirdaus@gmail.com>
2022-11-23 00:17:18 +0700

    Update DequeueNotification class inherits Toplevel daripada Tk

--- notif.py
+++ notif.py
@@ -1,18 +1,21 @@
 from tkinter import *
 from tkinter import ttk
 
-class DequeueNotification(Tk):
-    def __init__(self, data_pasien : list):
-        super().__init__()
+class DequeueNotification(Toplevel):
+    def __init__(self, master, data_pasien):
+        super().__init__(master)
         self.data_pasien = data_pasien
+        self.master = master
         self.title("Notifikasi")
         self.resizable(False, False)
         self.create_mainframe()
         self.add_style()
         self.create_nomor()
         self.create_tulisan()
-        self.after(3000, self.destroy)
-    
+        self.after(3000, lambda: self.destroy())
+        self.grab_set()
+        self.wait_window()
+        
     def create_mainframe(self):
         self.mainframe = ttk.Frame(self, padding=10)
         self.mainframe.grid(row=0, column=0, sticky="news",padx=10, pady=10)
@@ -22,7 +25,7 @@ class DequeueNotification(Tk):
     def create_nomor(self):
         no_container = ttk.Frame(self.mainframe)
         no_container.grid(row=0, column=0, sticky=W)
-        ttk.Label(no_container, text="21", font=("Times New Roman", 32, "bold"),padding=20, style="Nomor.TLabel").pack(padx=10)
+        ttk.Label(no_container, text=self.data_pasien[0], font=("Times New Roman", 32, "bold"),padding=20, style="Nomor.TLabel").pack(padx=10)
     
     def create_tulisan(self):
         tulisan_container = ttk.Frame(self.mainframe, padding=10)
@@ -37,8 +40,8 @@ class DequeueNotification(Tk):
         style.configure('Nomor.TLabel', background="#8d8daa")
 
 
-
 if __name__ == "__main__":
-    DequeueNotification([2, "daiod", "tulus"]).mainloop()
-
+    mas = Tk()
+    DequeueNotification(mas, [1, "e3e2", "eq"]).show_dialog()
+    mas.mainloop()
         
